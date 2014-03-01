7aa4dc0317c4fbfd6e05b75fdb27d83221352cb7
David Su <david.d.su@gmail.com>
2014-03-01 02:21:28 -0500

    got some 'oo'/'OO's for facebook

--- app/background.js
+++ app/background.js
@@ -1,2 +1,19 @@
-$("body").append("ho");
-console.log("hoo");
+var $j = jQuery.noConflict();
+$j("body").append("ho");
+console.log($j("span").text());
+
+/**
+    Retrieving Facebook elements (trial and error)
+
+    - "span" for user text elements (comments have weird names)
+    - ".userContent" for posts
+    - "._1x_" is link title
+    - "._1y1" is link desc
+    - "a" is comment links
+
+**/
+$j("span, a, ._1x_, .1y1, .userContent").each(function() {
+    var text = $j(this).text();
+    $j(this).text(text.replace(/[aeiou]/g, "oo")
+                    .replace(/[AEIOU]/g, "OO"));
+})
