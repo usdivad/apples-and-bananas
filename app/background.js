96850cbe5f38f442bcbda49f30c93f826f98cda4
David Su <david.d.su@gmail.com>
2014-03-01 02:49:01 -0500

    preserve <img> elements, create replace_vowels()

--- app/background.js
+++ app/background.js
@@ -1,6 +1,6 @@
 var $j = jQuery.noConflict();
 $j("body").append("ho");
-console.log($j("span").text());
+//console.log($j("span").text());
 
 /**
     Retrieving Facebook elements (trial and error)
@@ -13,7 +13,28 @@ console.log($j("span").text());
 
 **/
 $j("span, a, ._1x_, .1y1, .userContent").each(function() {
-    var text = $j(this).text();
-    $j(this).text(text.replace(/[aeiou]/g, "oo")
-                    .replace(/[AEIOU]/g, "OO"));
-})
+    var content_text = $j(this).text();
+    var content_html = $j(this).html();
+    if (content_html.match(/<img.*>/) === null) { //make sure we don't have images
+        $j(this).text(replace_vowels(content_text, "oo"));
+    }
+    else {
+        console.log(content_text);
+    }
+});
+
+function replace_vowels(str, vowel) {
+    var lower = vowel.toLowerCase();
+    var upper = vowel.toUpperCase();
+    var pattern_lower = /[aeiou]/g;
+    var pattern_upper = /[AEIOU]/g;
+
+    return str.replace(pattern_lower, lower).replace(pattern_upper, upper);
+}
+
+/*
+    TODO:
+    - Shuffle the intermediate characters (charles -> clraehs)
+    - Maintain photos n such (don't destroy)
+
+*/
