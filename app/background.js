6392a3a2f27395138c1af82aab166eedeb25f564
David Su <david.d.su@gmail.com>
2014-03-01 19:23:16 -0500

    using childNodes (method derived from Scramble) recursively instead of guessing at names

--- app/background.js
+++ app/background.js
@@ -1,19 +1,18 @@
 var $j = jQuery.noConflict();
+var TEXT_NODE = 3;
 //$j("body").append("ho");
 //console.log($j("span").text());
 
+/*
 function banana(vowel) {
     console.log("BANANA");
-    /**
-        Retrieving Facebook elements (trial and error)
 
-        - "span" for user text elements (comments have weird names)
-        - ".userContent" for posts
-        - "._1x_" is link title
-        - "._1y1" is link desc
-        - "a" is comment links
-
-    **/
+    // Retrieving Facebook elements (trial and error)
+    // - "span" for user text elements (comments have weird names)
+    // - ".userContent" for posts
+    // - "._1x_" is link title
+    // - "._1y1" is link desc
+    // - "a" is comment links
     $j("a, span, ._1x_, .1y1, .userContent").each(function() {
         
         var content_text = $j(this).text();
@@ -39,11 +38,44 @@ function replace_vowels(str, vowel) {
 
     return str.replace(pattern_lower, lower).replace(pattern_upper, upper);
 }
+*/
+
+
+function banana_text(str, vowel) {
+    var vowel_lower = vowel.toLowerCase();
+    var vowel_upper = vowel.toUpperCase();
+    var pattern_lower = /[aeiou]/g;
+    var pattern_upper = /[AEIOU]/g;
+    var control_lower = "a";
+    var control_upper = control_lower.toUpperCase();
+
+    var temp = str.replace(vowel_lower, control_lower).replace(vowel_upper, control_upper);
+
+    return temp.replace(pattern_lower, vowel_lower).replace(pattern_upper, vowel_upper);
+}
+
+
+function banana_nodes(nodes, vowel) {
+    for (var i=0; i<nodes.length; i++) {
+        if (nodes[i].nodeType == TEXT_NODE) {
+            nodes[i].nodeValue = banana_text(nodes[i].nodeValue, vowel);
+        }
+        else {
+            banana_nodes(nodes[i].childNodes, vowel); //the recursive call!
+        }
+    }
+}
+
+function banana_page(vowel) {
+    var nodes = document.getElementsByTagName("body");
+    banana_nodes(nodes, vowel);
+}
+
 
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         console.log(sender.tab);
-        banana(request.vowel);
+        banana_page(request.vowel);
         sendResponse({msg: "this is what's up"});
     }
 );
